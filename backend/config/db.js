88248568ed29fae8721df00a7bc86e4c74d2bea0
kinan-saad <kinan.saad.19@gmail.com>
2025-06-19 09:06:20 +0300

    changed db connection to local connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,28 +1,28 @@
-// external connection
-import mongoose from 'mongoose'
-
-export const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (error) {
-    console.log(`Error: ${error.message}`)
-    process.exit(1) // process 1 code means failure, 0 means success
-  }
-}
-
-// // locat connection
+// // external connection
 // import mongoose from 'mongoose'
-
-// // mongodb://localhost:27017
-
-// const uri = 'mongodb://127.0.0.1:27017/mern-project-1'
 
 // export const connectDB = async () => {
 //   try {
-//     const conn = await mongoose.connect(uri)
-//     console.log('DB is running...')
+//     const conn = await mongoose.connect(process.env.MONGO_URI)
+//     console.log(`MongoDB Connected: ${conn.connection.host}`)
 //   } catch (error) {
-//     console.log('Error: ', error)
+//     console.log(`Error: ${error.message}`)
+//     process.exit(1) // process 1 code means failure, 0 means success
 //   }
 // }
+
+// local connection
+import mongoose from 'mongoose'
+
+// mongodb://localhost:27017
+
+const uri = 'mongodb://127.0.0.1:27017/mern-project-1'
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(uri)
+    console.log('DB is running...')
+  } catch (error) {
+    console.log('Error: ', error)
+  }
+}
